94ac1191fd0fce280d024912e9f2a3bc17dbfa2a
chintamanil <chintamani.lonkar@gmail.com>
2016-05-16 12:39:56 -0700

    Added test for Player

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
     var chai = require('chai');
     var expect = chai.expect;
     var Game = require('./../src/game.js').Game;
+    var globals = require('./../src/globals.js');
     var GameEvent = require('./../src/gameEvent.js').Game;
     var Player = require('./../src/player.js').Player;
     var Board = require('./../src/board.js').Board;
@@ -10,11 +11,12 @@
     describe('Battleship game testing', function() {
         describe(' Ship test', function() {
 
-            beforeEach(function() {
+            before(function() {
                 game = new Game();
                 ship = new Ship(2);
                 board = new Board();
-                player = new Player('Player-1');
+                player1 = new Player('Player-1');
+                player2 = new Player('Player-2');
             });
 
             it('Check ship state before hit = X', function() {
@@ -50,19 +52,25 @@
                 expect(board.check(4, 0)).to.equal('Sunk');
             });
 
-            //   it('Board check sunk', function() {
-            //     game.init()
-            // });
-
         });
 
         describe(' Player test', function() {
-
-            // it('Create board & check Miss', function() {
-            //     var player = new Player('Player-1');
-            //     board.setShips([2, 2, 2, -1, -1]);
-            //     expect(board.check(0, 0)).to.equal('Miss');
-            // });
+            // Need to figure a way to test readline
+            it('Player 1 & 2 board setup & check ', function() {
+                player1.board.setShips([2, 2, 2, -1, -1]);
+                player2.board.setShips([0, 0, 0, -1, -1]);
+                expect(player1.board.check(0, 0)).to.equal('Miss');
+                expect(player2.board.check(0, 0)).to.equal('Hit');
+                expect(player2.board.check(1, 0)).to.equal('Hit');
+                expect(player2.board.check(2, 0)).to.equal('Sunk');
+                expect(player2.board.check(0, 1)).to.equal('Hit');
+                expect(player2.board.check(1, 1)).to.equal('Hit');
+                expect(player2.board.check(2, 1)).to.equal('Sunk');
+                expect(player2.board.check(0, 2)).to.equal('Hit');
+                expect(player2.board.check(1, 2)).to.equal('Hit');
+                expect(player2.board.check(2, 2)).to.equal('Sunk');
+                player2.board.printBoard(globals.write);
+            });
 
         });
     });
